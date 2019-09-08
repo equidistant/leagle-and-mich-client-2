dfa3f5ecfdbfe221a248f05f50b5d3c78576f87c
equidistant <mich.random@pm.me>
2019-09-08 20:02:07 +0200

    image height

--- src/components/blogs/components/Img.js
+++ src/components/blogs/components/Img.js
@@ -6,7 +6,7 @@ const Img = styled.img.attrs(props => ({
   }))`
   background-size: cover;
   width: 100%;
-  height: auto;
+  height: 300px;
   overflow: hidden;
   margin-right: ${props => props.right ? '0.5vw' : 0};
   margin-left: ${props => props.left ? '0.5vw' : 0};
